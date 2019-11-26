2fa7e21ae288c3b869396c285550a57993378db1
Samu Lang <langsamu@hotmail.com>
2019-11-26 12:27:19 +0000

    Add project files.

---
+++ GraphEngine/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GraphEngine
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
