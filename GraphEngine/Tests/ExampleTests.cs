ab86831d44d53cd9b96d83874b99fef2864bb280
Samu Lang <langsamu@hotmail.com>
2019-11-30 16:13:36 +0000

    Adds JSON Fiboacci example. Improves other examples.

--- GraphEngine/Tests/ExampleTests.cs
+++ GraphEngine/Tests/ExampleTests.cs
@@ -1,6 +1,7 @@
 namespace GraphEngine.Tests
 {
     using System;
+    using System.Linq;
     using System.Linq.Expressions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using VDS.RDF;
@@ -14,16 +15,7 @@ namespace GraphEngine.Tests
             using var g = new Graph();
             g.LoadFromEmbeddedResource("GraphEngine.Resources.Examples.FibonacciSequence.xml,GraphEngine");
 
-            var s = g.GetUriNode(":s");
-
-            var expression = ExpressionNode.Parse(s).Expression;
-            Console.WriteLine(expression.GetDebugView());
-
-            var lambda = Expression.Lambda(expression);
-            var actual = lambda.Compile().DynamicInvoke();
-
-            Assert.AreEqual(21L, actual);
-
+            NewMethod(g);
         }
 
         [TestMethod]
@@ -32,6 +24,22 @@ namespace GraphEngine.Tests
             using var g = new Graph();
             g.LoadFromEmbeddedResource("GraphEngine.Resources.Examples.FibonacciSequence.ttl,GraphEngine");
 
+            NewMethod(g);
+        }
+
+        [TestMethod]
+        public void FibonacciJson()
+        {
+            using var ts = new TripleStore();
+            ts.LoadFromEmbeddedResource("GraphEngine.Resources.Examples.FibonacciSequence.json,GraphEngine");
+            var g = ts.Graphs.Single();
+            g.NamespaceMap.AddNamespace(string.Empty, UriFactory.Create("http://example.com/"));
+
+            NewMethod(g);
+        }
+
+        private static void NewMethod(IGraph g)
+        {
             var s = g.GetUriNode(":s");
 
             var expression = ExpressionNode.Parse(s).Expression;
@@ -41,7 +49,6 @@ namespace GraphEngine.Tests
             var actual = lambda.Compile().DynamicInvoke();
 
             Assert.AreEqual(21L, actual);
-
         }
     }
 }
